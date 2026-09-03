e3dcd8f500c810d3d412591e74244cfd7c9bb8e6
reachoutsahani <ajaysahani6392@gmail.com>
2026-09-03 10:40:08 +0530

    Fix duplicate BrowserRouter

--- Frontend/src/AppRoutes.jsx
+++ Frontend/src/AppRoutes.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
